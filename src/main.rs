cdc07fce114fbbdc202437c4de32fa76b4f3e972
Anton Strand <anton.strand@insurello.se>
2021-12-02 21:30:54 +0100

    Refactor parsing day

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,29 @@
 use std::env;
 
-use advent_of_code::{day_01, day_02, Solution};
+use advent_of_code::{day_01, day_02, day_03, Solution};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let days = [day_01, day_02];
+    let days = [day_01, day_02, day_03];
 
-    if let Some(day) = args.get(1) {
-        let index: usize = day.parse::<usize>().unwrap() - 1;
-        days.get(index)
-            .map(solve)
-            .unwrap_or_else(|| println!("You need to provide a day between 1 - {}", days.len()))
-    } else {
-        days.iter().for_each(solve);
+    match parse_arg_day() {
+        Some(day) => solve_day(&days, day),
+        None => days.iter().for_each(solve),
     }
 }
 
-fn solve<T>(solve: T)
-where
-    T: Fn() -> Solution,
-{
+fn parse_arg_day() -> Option<usize> {
+    env::args()
+        .collect::<Vec<String>>()
+        .get(1)
+        .map(|day| day.parse::<usize>().unwrap() - 1)
+}
+
+fn solve_day<F: Fn() -> Solution>(days: &[F], day: usize) {
+    days.get(day)
+        .map(solve)
+        .unwrap_or_else(|| println!("You need to provide a day between 1 - {}", days.len()))
+}
+
+fn solve<F: Fn() -> Solution>(solve: F) {
     println!("{}", solve());
 }
