d3a47dfc691ff4717ad8f40b1c0832ddf84abcda
Anton Strand <anton.strand@insurello.se>
2021-12-15 09:14:50 +0100

    Refactor after learning new stuff

--- src/day_01.rs
+++ src/day_01.rs
@@ -1,33 +1,33 @@
 use crate::{read_number_input, Solution};
 
-fn increment(current: u32, x: u32, y: u32) -> u32 {
-    if x > y {
-        current + 1
-    } else {
-        current
-    }
+fn part01(input: &Vec<u16>) -> usize {
+    input.windows(2).filter(|n| n[0] < n[1]).count()
 }
 
-fn part01(input: &Vec<u32>) -> u64 {
-    let first = input[0];
-    let rest = &input[1..];
-    rest.iter()
-        .fold((first, 0), |(prev, count), x| {
-            (*x, increment(count, *x, prev))
-        })
-        .1 as u64
-}
-
-fn part02(input: &Vec<u32>) -> u64 {
-    let values: Vec<u32> = (0..(input.len() - 2))
-        .map(|i| input[i..(i + 3)].iter().sum())
-        .collect();
-
-    part01(&values)
+fn part02(input: &Vec<u16>) -> usize {
+    input.windows(4).filter(|n| n[0] < n[3]).count()
 }
 
 pub fn day_01() -> Solution {
     let input = read_number_input("./input/day_01.txt");
     let timer = std::time::Instant::now();
     Solution::new(1, part01(&input), part02(&input), timer.elapsed())
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_part01() {
+        let input = read_number_input("./input/day_01.txt");
+        assert_eq!(part01(&input), 1759)
+    }
+
+    #[test]
+    fn test_part02() {
+        let input = read_number_input("./input/day_01.txt");
+        assert_eq!(part02(&input), 1805)
+    }
 }
