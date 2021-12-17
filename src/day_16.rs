055d3cdacb44f2110dde69e81994e5f34797f343
Anton Strand <anton.strand@insurello.se>
2021-12-17 10:00:41 +0100

    Solve day 16

--- src/day_16.rs
+++ src/day_16.rs
@@ -1,234 +1,108 @@
-use crate::{helpers::map_fst, read, Solution};
+use crate::{read, Solution};
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
-enum Operator {
-    Sum,
-    Product,
-    Minimum,
-    Maximum,
-    Gt,
-    Lt,
-    Eq,
-}
-
-impl Operator {
-    fn new(operator: &str) -> Self {
-        match from_binary(operator) {
-            0 => Self::Sum,
-            1 => Self::Product,
-            2 => Self::Minimum,
-            3 => Self::Maximum,
-            5 => Self::Gt,
-            6 => Self::Lt,
-            _ => Self::Eq,
-        }
-    }
-
-    fn apply(&self, values: Vec<u64>) -> u64 {
-        let eval = |boolean| if boolean { 1 } else { 0 };
-        let vals = values.clone().into_iter();
-        println!("Apply {:?} to {:?}", self, values);
-        match self {
-            Operator::Sum => vals.sum(),
-            Operator::Product => vals.product(),
-            Operator::Minimum => vals.min().unwrap(),
-            Operator::Maximum => vals.max().unwrap(),
-            Operator::Gt => eval(values.get(0).unwrap() > values.get(1).unwrap()),
-            Operator::Lt => eval(values.get(0).unwrap() < values.get(1).unwrap()),
-            Operator::Eq => eval(values.get(0).unwrap() == values.get(1).unwrap()),
-        }
-    }
-}
-
-#[derive(Debug, PartialEq, Eq, Clone)]
-struct OperatorData {
-    version: u64,
-    operator: Operator,
-    subpackets: Vec<Packet>,
-}
-
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
-struct LiteralData {
-    version: u64,
-    value: u64,
-}
-
-#[derive(PartialEq, Eq, Clone)]
-enum Packet {
-    Literal(LiteralData),
-    Operator(OperatorData),
-}
-
-impl Packet {
-    fn new_literal(version: u64, value: u64) -> Self {
-        Packet::Literal(LiteralData { version, value })
-    }
-
-    fn new_operator(version: u64, operator: Operator, subpackets: Vec<Self>) -> Self {
-        Packet::Operator(OperatorData {
-            version,
-            subpackets,
-            operator,
-        })
-    }
-
-    fn version(packet: Packet) -> u64 {
-        match packet {
-            Self::Literal(data) => data.version,
-            Self::Operator(data) => {
-                data.version
-                    + data
-                        .subpackets
-                        .into_iter()
-                        .map(Packet::version)
-                        .sum::<u64>()
-            }
-        }
-    }
-
-    fn calculate(packet: Packet) -> u64 {
-        match packet {
-            Self::Literal(data) => data.value,
-            Self::Operator(data) => {
-                let values = data
-                    .subpackets
-                    .clone()
-                    .into_iter()
-                    .map(Packet::calculate)
-                    .collect();
-
-                data.operator.apply(values)
-            }
-        }
-    }
-}
-
-impl std::fmt::Debug for Packet {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::Literal(data) => write!(f, "(Literal {})", data.value),
-            Self::Operator(data) => {
-                write!(f, "(Operator {:?}, {:#?})", data.operator, data.subpackets)
-            }
-        }
-    }
-}
-
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
-enum TypeId {
-    Literal,
-    Operator(Operator),
-}
-
-impl TypeId {
-    fn from_str(s: &str) -> Self {
-        match s {
-            "100" => Self::Literal,
-            _ => Self::Operator(Operator::new(s)),
-        }
-    }
-}
-
-fn from_binary(binary: &str) -> u64 {
-    u64::from_str_radix(binary, 2).unwrap()
-}
-
-fn usize_from_binary(binary: &str) -> usize {
-    usize::from_str_radix(binary, 2).unwrap()
-}
-
-// This would have been better suited for a library but I want to do it myself
 fn hex_to_binary(hex: &str) -> String {
-    hex.to_uppercase()
-        .chars()
-        .map(|c| match c {
-            '0' => "0000",
-            '1' => "0001",
-            '2' => "0010",
-            '3' => "0011",
-            '4' => "0100",
-            '5' => "0101",
-            '6' => "0110",
-            '7' => "0111",
-            '8' => "1000",
-            '9' => "1001",
-            'A' => "1010",
-            'B' => "1011",
-            'C' => "1100",
-            'D' => "1101",
-            'E' => "1110",
-            'F' => "1111",
-            _ => panic!("Not hex: {}", c),
-        })
-        .collect::<Vec<&str>>()
-        .join("")
+    hex.chars()
+        // :04b turn number to binary and pad with preceding 0 to a total width of 4
+        .map(|c| format!("{:04b}", u8::from_str_radix(&c.to_string(), 16).unwrap()))
+        .collect()
 }
 
-fn parse(max_packet: usize, mut start_binary: &str) -> (Vec<Packet>, &str) {
-    let mut packets = vec![];
+fn eval(op: &str, literals: Vec<u64>) -> u64 {
+    match op {
+        "000" => literals.iter().sum(),
+        "001" => literals.iter().product(),
+        "010" => *literals.iter().min().unwrap(),
+        "011" => *literals.iter().max().unwrap(),
+        "101" => (literals[0] > literals[1]) as u64,
+        "110" => (literals[0] < literals[1]) as u64,
+        "111" => (literals[0] == literals[1]) as u64,
+        _ => unreachable!(),
+    }
+}
 
+fn parse_literal(binary: &str, mut pos: usize) -> (u64, usize) {
+    let mut value = String::new();
     loop {
-        if start_binary.trim_end_matches('0').is_empty() || packets.len() == max_packet {
-            break;
+        let bits = &binary[pos..pos + 5];
+        pos += 5;
+        value.push_str(&bits[1..]);
+
+        if bits.starts_with("0") {
+            return (u64::from_str_radix(&value, 2).unwrap(), pos);
         }
+    }
+}
 
-        let (version, bin) = map_fst(from_binary, start_binary.split_at(3));
-        let (type_id, mut binary) = map_fst(TypeId::from_str, bin.split_at(3));
+fn parse_by_sub_count(binary: &str, mut pos: usize) -> (usize, u64, Vec<u64>) {
+    let num_sub_packets = usize::from_str_radix(&binary[pos + 1..pos + 12], 2).unwrap();
+    pos += 12;
+    let mut version = 0;
+    let results = (0..num_sub_packets)
+        .into_iter()
+        .map(|_| {
+            let (p, v, r) = parse(binary, pos);
+            pos = p;
+            version += v;
+            r
+        })
+        .collect();
 
-        match type_id {
-            TypeId::Literal => {
-                let mut literal = "".to_string();
-                let mut bin = binary;
-                loop {
-                    let (part, rest) = bin.split_at(5);
-                    literal = literal + &part[1..];
-                    bin = rest;
-                    if part.starts_with("0") {
-                        packets.push(Packet::new_literal(version, from_binary(literal.as_str())));
-                        break;
-                    }
-                }
-                binary = bin;
-            }
-            TypeId::Operator(operator) => {
-                let (bits, bin) =
-                    map_fst(|t| if t == "0" { 15usize } else { 11 }, binary.split_at(1));
+    (pos, version, results)
+}
 
-                let (sub_len, rest) = map_fst(usize_from_binary, bin.split_at(bits));
-                let (subpackets, remainer) = parse(sub_len, rest);
+fn parse_by_bits(binary: &str, mut pos: usize) -> (usize, u64, Vec<u64>) {
+    let bits = usize::from_str_radix(&binary[pos + 1..pos + 16], 2).unwrap();
+    pos += 16;
+    let stop_at = pos + bits;
+    let mut version = 0;
+    let mut results = vec![];
 
-                packets.push(Packet::new_operator(version, operator, subpackets));
-
-                binary = remainer;
-            }
-        }
-
-        start_binary = binary;
+    while pos < stop_at {
+        let (p, v, r) = parse(binary, pos);
+        pos = p;
+        version += v;
+        results.push(r);
     }
 
-    (packets, start_binary)
+    (pos, version, results)
+}
+
+fn parse(binary: &str, mut pos: usize) -> (usize, u64, u64) {
+    // No more packet can be parsed
+    if pos + 6 > binary.len() {
+        return (binary.len(), 0, 0);
+    }
+    let mut version = u64::from_str_radix(&binary[pos..pos + 3], 2).unwrap();
+    let type_id = &binary[pos + 3..pos + 6];
+    pos += 6;
+
+    let result = if type_id == "100" {
+        let (literal, p) = parse_literal(binary, pos);
+        pos = p;
+        literal
+    } else {
+        // operator
+        let (p, v, results) = if &binary[pos..pos + 1] == "1" {
+            parse_by_sub_count(binary, pos)
+        } else {
+            parse_by_bits(binary, pos)
+        };
+
+        pos = p;
+        version += v;
+        eval(type_id, results)
+    };
+    (pos, version, result)
 }
 
 /* Solutions */
 
 fn part01(input: &String) -> u64 {
-    parse(usize::MAX, &input)
-        .0
-        .into_iter()
-        .map(Packet::version)
-        .sum()
+    parse(&input, 0).1
 }
 
 fn part02(input: &String) -> u64 {
-    // parse(usize::MAX, &input)
-    //     .0
-    //     .into_iter()
-    //     .for_each(|p| println!("{:?}", p));
-    parse(usize::MAX, &input)
-        .0
-        .into_iter()
-        .map(Packet::calculate)
-        .sum::<u64>()
+    parse(&input, 0).2
 }
 
 pub fn day_16() -> Solution {
@@ -245,7 +119,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_hex_to_binary_D2FE28() {
+    fn test_hex_to_binary_d2fe28() {
         assert_eq!(
             hex_to_binary("D2FE28"),
             String::from("110100101111111000101000")
@@ -253,7 +127,7 @@ mod tests {
     }
 
     #[test]
-    fn test_hex_to_binary_38006F45291200() {
+    fn test_hex_to_binary_38006f45291200() {
         assert_eq!(
             hex_to_binary("38006F45291200"),
             String::from("00111000000000000110111101000101001010010001001000000000")
@@ -261,22 +135,22 @@ mod tests {
     }
 
     #[test]
-    fn test_part_1_8A004A801A8002F478() {
+    fn test_part_1_8a004a801a8002f478() {
         assert_eq!(part01(&hex_to_binary("8A004A801A8002F478")), 16)
     }
 
     #[test]
-    fn test_part_1_620080001611562C8802118E34() {
+    fn test_part_1_620080001611562c8802118e34() {
         assert_eq!(part01(&hex_to_binary("620080001611562C8802118E34")), 12)
     }
 
     #[test]
-    fn test_part_1_C0015000016115A2E0802F182340() {
+    fn test_part_1_c0015000016115a2e0802f182340() {
         assert_eq!(part01(&hex_to_binary("C0015000016115A2E0802F182340")), 23)
     }
 
     #[test]
-    fn test_part_1_A0016C880162017C3686B18A3D4780() {
+    fn test_part_1_a0016c880162017c3686b18a3d4780() {
         assert_eq!(part01(&hex_to_binary("A0016C880162017C3686B18A3D4780")), 31)
     }
 
@@ -329,6 +203,6 @@ mod tests {
     #[test]
     fn test_part02() {
         let input = &hex_to_binary(&read("./input/day_16.txt"));
-        assert_eq!(part02(&input), 2914)
+        assert_eq!(part02(&input), 184487454837)
     }
 }
