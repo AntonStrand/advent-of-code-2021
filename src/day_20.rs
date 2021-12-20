95a91e0725101c796c783fa0c0e0f40d2b9146c7
Anton Strand <anton.strand@insurello.se>
2021-12-21 00:13:22 +0100

    Solve day 20

--- src/day_20.rs
+++ src/day_20.rs
@@ -2,26 +2,21 @@ use std::{collections::HashMap, fmt::Debug};
 
 use crate::{map_both, read, Solution};
 
-type Algorithm = [char; 512];
+type Algorithm = [bool; 512];
 struct Image {
-    pixels: HashMap<(i32, i32), char>,
-    width: usize,
-    height: usize,
+    pixels: HashMap<(isize, isize), bool>,
+    size: isize,
     lit: u32,
+    default_pixel: bool,
 }
 
 impl Image {
-    fn copy(&self) -> Image {
+    fn new(size: isize, default_pixel: bool) -> Image {
         Image {
-            pixels: self
-                .pixels
-                .clone()
-                .into_iter()
-                .map(|((x, y), v)| ((x + 1, y + 1), v))
-                .collect(),
-            width: self.width + 2,
-            height: self.width + 2,
+            pixels: HashMap::new(),
+            size,
             lit: 0,
+            default_pixel,
         }
     }
 
@@ -36,24 +31,21 @@ impl Image {
                     line.chars()
                         .enumerate()
                         .map(|(x, c)| {
-                            ((1 + x as i32, 1 + y as i32), {
+                            ((x as isize, y as isize), {
                                 lit += (c == '#') as u32;
-                                char_to_binary(c)
+                                is_lit(c)
                             })
                         })
-                        .collect::<Vec<((i32, i32), char)>>()
+                        .collect::<Vec<((isize, isize), bool)>>()
                 })
                 .collect(),
-            width: input.lines().last().unwrap().len(),
-            height: input.lines().count(),
+            size: input.lines().count() as isize,
             lit,
+            default_pixel: false,
         }
     }
 
-    fn get_algorithm_index(&self, x: usize, y: usize) -> usize {
-        let x = x as i32;
-        let y = y as i32;
-
+    fn get_algorithm_index(&self, x: isize, y: isize, default_pixel: bool) -> usize {
         let binary: String = [
             (x - 1, y - 1),
             (x, y - 1),
@@ -65,69 +57,73 @@ impl Image {
             (x, y + 1),
             (x + 1, y + 1),
         ]
-        .map(|pos| *self.pixels.get(&pos).unwrap_or(&'0'))
+        .map(|pos| *self.pixels.get(&pos).unwrap_or(&default_pixel))
         .iter()
-        .fold("".to_string(), |b, c| b + &c.to_string());
+        .fold(String::new(), |s, b| format!("{}{}", s, *b as u8));
+
+        // println!("{}", binary);
 
         usize::from_str_radix(&binary, 2).unwrap()
     }
 
-    fn insert(&mut self, x: usize, y: usize, pixel: char) {
-        self.pixels.insert((x as i32, y as i32), pixel);
-        self.lit += pixel.to_digit(16).unwrap()
+    fn insert(&mut self, x: isize, y: isize, pixel: bool) {
+        self.pixels.insert((x, y), pixel);
+        self.lit += pixel as u32
+    }
+
+    fn enhance(&self, algorithm: Algorithm) -> Image {
+        let mut output = Image::new(self.size + 2, !self.default_pixel);
+
+        for y in 0..output.size {
+            for x in 0..output.size {
+                let i = self.get_algorithm_index(x - 1, y - 1, self.default_pixel);
+                output.insert(x, y, algorithm[i]);
+            }
+        }
+
+        output
     }
 }
 
 impl Debug for Image {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut grid: Vec<_> = self.pixels.clone().into_iter().collect();
+
+        grid.sort_by_key(|&((x, y), _)| (y, x));
+
+        let grid = grid
+            .into_iter()
+            .fold((String::new(), 0), |(mut s, row), ((_, y), b)| {
+                s.push_str(if b { "#" } else { " " });
+
+                if row < y {
+                    s.push_str("\n");
+                }
+
+                (s, y)
+            })
+            .0;
+
         write!(
             f,
-            "{}\nWidth: {}\nHeight: {}\nLit: {}",
-            self.pixels
-                .clone()
-                .into_iter()
-                .fold([[" "; 105]; 105], |mut img, ((x, y), pixel)| {
-                    img[y as usize][x as usize] = if pixel == '1' { "#" } else { " " };
-                    img
-                })
-                .map(|line| line.join(""))
-                .join("\n"),
-            self.width,
-            self.height,
-            self.lit
+            "{:?}\nSize: {}x{}\nLit: {}",
+            grid, self.size, self.size, self.lit
         )
     }
 }
 
-fn char_to_binary(c: char) -> char {
-    if c == '.' {
-        '0'
-    } else {
-        '1'
-    }
+fn is_lit(c: char) -> bool {
+    c == '#'
 }
 
 fn parse_algorithm(input: &str) -> Algorithm {
-    let mut algo = ['.'; 512];
+    let mut algo = [false; 512];
 
     for (i, c) in input.chars().enumerate() {
-        algo[i] = char_to_binary(c);
+        algo[i] = is_lit(c);
     }
 
     algo
-}
-
-fn enhance(image: Image, algorithm: Algorithm) -> Image {
-    let mut output = image.copy();
-
-    for y in 0..output.height {
-        for x in 0..output.width {
-            let i = image.get_algorithm_index(x, y);
-            output.insert(x + 1, y + 1, algorithm[i]);
-        }
-    }
-
-    output
 }
 
 /* Solutions */
@@ -139,15 +135,21 @@ fn part01(input: &String) -> u32 {
         input.split_once("\n\n").unwrap(),
     );
 
-    let res = enhance(enhance(image, algorithm), algorithm);
-
-    println!("{:?}", res);
-
-    res.lit
+    image.enhance(algorithm).enhance(algorithm).lit
 }
 
-fn part02(_input: &String) -> u16 {
-    0
+fn part02(input: &String) -> u32 {
+    let (algorithm, mut image) = map_both(
+        parse_algorithm,
+        Image::from_str,
+        input.split_once("\n\n").unwrap(),
+    );
+
+    for _ in 0..50 {
+        image = image.enhance(algorithm);
+    }
+
+    image.lit
 }
 
 pub fn day_20() -> Solution {
@@ -164,11 +166,6 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_parse_algorithm() {
-        assert_eq!(parse_algorithm("..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..###..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#..#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#......#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.....####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.......##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#"), ['0', '0', '1', '0', '1', '0', '0', '1', '1', '1', '1', '1', '0', '1', '0', '1', '0', '1', '0', '1', '1', '1', '0', '1', '1', '0', '0', '0', '0', '0', '1', '1', '1', '0', '1', '1', '0', '1', '0', '0', '1', '1', '1', '0', '1', '1', '1', '1', '0', '0', '1', '1', '1', '1', '1', '0', '0', '1', '0', '0', '0', '0', '1', '0', '0', '1', '0', '0', '1', '1', '0', '0', '1', '1', '1', '0', '0', '1', '1', '1', '1', '1', '1', '0', '1', '1', '1', '0', '0', '0', '1', '1', '1', '1', '0', '0', '1', '0', '0', '1', '1', '1', '1', '1', '0', '0', '1', '1', '0', '0', '1', '0', '1', '1', '1', '1', '1', '0', '0', '0', '1', '1', '0', '1', '0', '1', '0', '0', '1', '0', '1', '1', '0', '0', '1', '0', '1', '0', '0', '0', '0', '0', '0', '1', '0', '1', '1', '1', '0', '1', '1', '1', '1', '1', '1', '0', '1', '1', '1', '0', '1', '1', '1', '1', '0', '0', '0', '1', '0', '1', '1', '0', '1', '1', '0', '0', '1', '0', '0', '1', '0', '0', '1', '1', '1', '1', '1', '0', '0', '0', '0', '0', '1', '0', '1', '0', '0', '0', '0', '1', '1', '1', '0', '0', '1', '0', '1', '1', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '1', '0', '0', '1', '0', '0', '1', '0', '0', '1', '1', '0', '0', '1', '0', '0', '0', '1', '1', '0', '1', '1', '1', '1', '1', '1', '0', '1', '1', '1', '1', '0', '1', '1', '1', '1', '0', '1', '0', '1', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '1', '0', '1', '0', '1', '0', '0', '0', '1', '1', '1', '1', '0', '1', '1', '0', '1', '0', '0', '0', '0', '0', '0', '1', '0', '0', '1', '0', '0', '0', '1', '1', '0', '1', '0', '1', '1', '0', '0', '1', '0', '0', '0', '1', '1', '0', '1', '0', '1', '1', '0', '0', '1', '1', '1', '0', '1', '0', '0', '0', '0', '0', '0', '1', '0', '1', '0', '0', '0', '0', '0', '0', '0', '1', '0', '1', '0', '1', '0', '1', '1', '1', '1', '0', '1', '1', '1', '0', '1', '1', '0', '0', '0', '1', '0', '0', '0', '0', '0', '1', '1', '1', '1', '0', '1', '0', '0', '1', '0', '0', '1', '0', '1', '1', '0', '1', '0', '0', '0', '0', '1', '1', '0', '0', '1', '0', '1', '1', '1', '1', '0', '0', '0', '0', '1', '1', '0', '0', '0', '1', '1', '0', '0', '1', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '1', '0', '1', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '1', '1', '0', '0', '1', '1', '1', '1', '0', '0', '1', '0', '0', '0', '1', '0', '1', '0', '1', '0', '0', '0', '1', '1', '0', '0', '1', '0', '1', '0', '0', '1', '1', '1', '0', '0', '1', '1', '1', '1', '1', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '1', '1', '1', '1', '0', '0', '0', '0', '0', '0', '1', '0', '0', '1'])
-    }
-
-    #[test]
     fn test_get_algorithm_index() {
         let image = Image::from_str(
             r#"#..#.
@@ -177,20 +174,18 @@ mod tests {
 ..#..
 ..###"#,
         );
-        assert_eq!(image.get_algorithm_index(3, 3), 34)
+        assert_eq!(image.get_algorithm_index(2, 2, false), 34)
     }
 
     #[test]
     fn test_part01() {
         let input = read("./input/day_20.txt");
-        assert_eq!(part01(&input), 0)
+        assert_eq!(part01(&input), 5432)
     }
-    // < 5814
-    // < 5686
 
     #[test]
     fn test_part02() {
         let input = read("./input/day_20.txt");
-        assert_eq!(part02(&input), 0)
+        assert_eq!(part02(&input), 16016)
     }
 }
